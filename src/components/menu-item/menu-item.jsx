958b63966047eac9d8f9bdf02ed0ccd50223f751
Artur Gawron <artur@gawron.me>
2020-07-24 13:35:38 +0200

    Add menu-item component

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,2 +1,13 @@
 import React from 'react'
 import './menu-item.scss'
+
+const MenuItem = ({ title }) => (
+  <div className="menu-item">
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">Kup Teraz</span>
+    </div>
+  </div>
+)
+
+export default MenuItem
