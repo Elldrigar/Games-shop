009f0d92ef6084f695d3c5b36f9b09e3d4b95e10
Artur Gawron <artur@gawron.me>
2020-08-28 14:51:06 +0200

    Install and config prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div>
-      Hello Elldrigar!
-    </div>
-  );
+  return <div>Hello Elldrigar!</div>
 }
 
-export default App;
+export default App
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,2 +1,15 @@
 import React from 'react'
 import './homepages.scss'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Playstation</h1>
+          <span className='subtitle'>Kup Teraz</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
