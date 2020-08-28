bf9b9e1365b64515a9ec1e326669d374e84d59da
Artur Gawron <artur@gawron.me>
2020-08-28 22:16:18 +0200

    add background class name

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './menu-item.scss';
 
-const MenuItem = ({ title, imageUrl }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
   <div
     style={{
       backgroundImage: `url(${imageUrl}`,
     }}
-    className='menu-item'
+    className={`${size} menu-item`}
   >
+    <div
+      className='background-image'
+      style={{ backgroundImage: `url(${imageUrl}` }}
+    />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>Kup Teraz</span>
