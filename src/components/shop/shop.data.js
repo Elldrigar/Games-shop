2a60c1a5a5634ca2fa4572db6ff902dfa3ce12aa
Artur Gawron <artur@gawron.me>
2020-08-29 20:27:50 +0200

    Add hardcoded data SHOP_DATA

---
+++ src/components/shop/shop.data.js
@@ -0,0 +1,67 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: 'Sega',
+    routeName: 'sega',
+    items: [
+      {
+        id: 1,
+        name: 'Game_1',
+        imageUrl: '',
+        price: 249,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Xbox',
+    routeName: 'xbox',
+    items: [
+      {
+        id: 1,
+        name: 'Game_1',
+        imageUrl: '',
+        price: 249,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Nintendo',
+    routeName: 'nintendo',
+    items: [
+      {
+        id: 1,
+        name: 'Game_1',
+        imageUrl: '',
+        price: 249,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Playstation',
+    routeName: 'playstation',
+    items: [
+      {
+        id: 1,
+        name: 'Game_1',
+        imageUrl: '',
+        price: 249,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Atari',
+    routeName: 'atari',
+    items: [
+      {
+        id: 1,
+        name: 'Game_1',
+        imageUrl: '',
+        price: 249,
+      },
+    ],
+  },
+];
