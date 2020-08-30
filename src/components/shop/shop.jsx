fd9958597210ba516d43137fcc44f779a57cda1e
Artur Gawron <artur@gawron.me>
2020-08-30 18:17:07 +0200

    Add render whole collection

--- src/components/shop/shop.jsx
+++ src/components/shop/shop.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import SHOP_DATA from './shop.data';
+import CollectionPreview from '../collection-preview/collection-preview';
 
 class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+		this.state = {
+			collections: SHOP_DATA,
+		};
+	}
 
-  render() {
-    return <div>SHOP PAGE</div>;
-  }
+	render() {
+		const {collections} = this.state;
+		return (<div className='shop-page'>
+				{
+					collections.map(({id, ...otherCollectionProps}) => (
+						<CollectionPreview key={id} {...otherCollectionProps} />
+					))
+				}
+			</div>
+
+		)
+	}
 }
 
 export default ShopPage;
