ea1e759faf365eece161debc5aaaebfb6bebeb7d
Artur Gawron <artur@gawron.me>
2020-08-31 22:31:34 +0200

    Add single item component

---
+++ src/components/collection-item/collection-item.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './collection-item.scss';
+
+const baseUrl = 'https://demo.gawron.me/img/gamecovers';
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+  <div className='collection-item'>
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${baseUrl}${imageUrl}.jpg`,
+      }}
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
+    </div>
+  </div>
+);
+
+export default CollectionItem;
