dfa152a01f2f9492cbacf2096393e6055d2f9c32
Artur Gawron <artur@gawron.me>
2020-09-05 00:11:08 +0200

    Add google signIn and signOut

--- src/App.js
+++ src/App.js
@@ -5,20 +5,44 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './components/shop/shop';
 import Header from './components/header/header';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInSignUpPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Header currentUser={ this.state.currentUser } />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInSignUpPage} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
